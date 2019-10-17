0e0d405726b22b5349e648c916db679c88ae034a
jain C <at3158@hotmail.com>
2019-10-17 17:05:35 +0800

    DataType(DataType.Date)

--- TKWEB/Models/Hridworkhrs.cs
+++ TKWEB/Models/Hridworkhrs.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TKWEB.Models
 {
     public partial class Hridworkhrs
     {
         public Guid Id { get; set; }
-        
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}",ApplyFormatInEditMode = true)]
         public DateTime? Wrokdates { get; set; }
         public string Loginid { get; set; }
         public string Workid { get; set; }
