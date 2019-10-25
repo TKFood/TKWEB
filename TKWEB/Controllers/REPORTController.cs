db804013c442b67a4ca2df879cb9c2365b649ad9
jain C <at3158@hotmail.com>
2019-10-25 11:08:53 +0800

    起始日期 格式=年月日

--- TKWEB/Controllers/REPORTController.cs
+++ TKWEB/Controllers/REPORTController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -25,33 +26,34 @@ namespace TKWEB.Controllers
         public async Task<IActionResult> Index(string STARTDATES,string ENDDATES)
         {
             string query=null;
+           
 
             var IdentityNAME = User.Identity.Name;
             var IdentityNAMEARRAY = IdentityNAME.ToString().Split("@");
             var SUERNAME = IdentityNAMEARRAY[0].ToString();
 
             var user = new SqlParameter("user", SUERNAME);
+            var sdats = new SqlParameter("sdats", STARTDATES);
+            var edats = new SqlParameter("edats", ENDDATES);
 
-            //ViewBag.account = Request.Form["STARTDATES"];
-            //ViewBag.account = Request.Form["ENDDATES"];
 
-            if(!String.IsNullOrEmpty(STARTDATES)&& !String.IsNullOrEmpty(ENDDATES))
+            if (!String.IsNullOrEmpty(STARTDATES)&& !String.IsNullOrEmpty(ENDDATES))
             {
-                query = "SELECT TOP 10 [ID],[WROKDATES],[LOGINID],[WORKID],[HRS] FROM [TKWEB].[dbo].[HRIDWORKHRS]  WHERE [LOGINID]=@user AND CONVERT(NVARCHAR,[WROKDATES],112) LIKE SUBSTRING(CONVERT(NVARCHAR,GETDATE(),112),1,6)+'%'  ORDER BY [WROKDATES] DESC ";
+                query = "SELECT [ID],[WROKDATES],[LOGINID],[WORKID],[HRS] FROM [TKWEB].[dbo].[HRIDWORKHRS]  WHERE [LOGINID]=@user AND CONVERT(NVARCHAR,[WROKDATES],112)>=@sdats AND CONVERT(NVARCHAR,[WROKDATES],112)<=@edats  ORDER BY [WROKDATES] DESC ";
+                var result = await _context.Hridworkhrs.FromSqlRaw(query, user, sdats, edats).ToListAsync();
+
+                return View(result);
+
             }
             else 
             {
                 query = "SELECT TOP 1 [ID],[WROKDATES],[LOGINID],[WORKID],[HRS] FROM [TKWEB].[dbo].[HRIDWORKHRS]  WHERE [LOGINID]=@user AND CONVERT(NVARCHAR,[WROKDATES],112) LIKE SUBSTRING(CONVERT(NVARCHAR,GETDATE(),112),1,6)+'%'  ORDER BY [WROKDATES] DESC ";
+                var result = await _context.Hridworkhrs.FromSqlRaw(query, user).ToListAsync();
+
+                return View(result);
             }
            
-           
-         
             
-
-            var result = await _context.Hridworkhrs.FromSqlRaw(query, user).ToListAsync();
-
-
-            return View(result);
 
             //return View(await _context.Hridworkhrs.Where(s => s.Loginid == SUERNAME.ToString()).ToListAsync());
             //return View(await _context.Hridworkhrs.ToListAsync());
