b3e54fac40e36cf066af16d0470013dcc78c2b0f
jain C <at3158@hotmail.com>
2019-10-25 14:35:10 +0800

    DisplayName

--- TKWEB/Models/Hridworkhrs.cs
+++ TKWEB/Models/Hridworkhrs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace TKWEB.Models
@@ -9,12 +10,16 @@ namespace TKWEB.Models
         public Guid Id { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}",ApplyFormatInEditMode = true)]
+        [DisplayName("日期")]
         [Required]
         public DateTime? Wrokdates { get; set; }
+        [DisplayName("工號")]
         [Required]
         public string Loginid { get; set; }
+        [DisplayName("工作項目")]
         [Required]
         public string Workid { get; set; }
+        [DisplayName("工時")]
         [Required]
         public decimal? Hrs { get; set; }
     }
