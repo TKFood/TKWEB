7541db4a542414f1d07cbd0af3f9b29647b5714c
jain C <at3158@hotmail.com>
2020-04-23 15:33:21 +0800

    DEL

--- TKWEB/Models/QUESTIONNAIRES.cs
+++ TKWEB/Models/QUESTIONNAIRES.cs
@@ -23,6 +23,10 @@ namespace TKWEB.Models
         [Required]
         public string NAME { get; set; }
 
+        [DisplayName("部門")]
+        [Required]
+        public string DEP { get; set; }
+
         [DisplayName("問題1")]
         [Required]
         public string QUESTION1 { get; set; }
@@ -30,5 +34,41 @@ namespace TKWEB.Models
         [DisplayName("問題2")]
         [Required]
         public string QUESTION2 { get; set; }
+
+        [DisplayName("問題3")]
+        [Required]
+        public string QUESTION3 { get; set; }
+
+        [DisplayName("問題4")]
+        [Required]
+        public string QUESTION4 { get; set; }
+
+        [DisplayName("問題5")]
+        [Required]
+        public string QUESTION5 { get; set; }
+
+        [DisplayName("問題6")]
+        [Required]
+        public string QUESTION6 { get; set; }
+
+        [DisplayName("問題7")]
+        [Required]
+        public string QUESTION7 { get; set; }
+
+        [DisplayName("問題8")]
+        [Required]
+        public string QUESTION8 { get; set; }
+
+        [DisplayName("問題9")]
+        [Required]
+        public string QUESTION9 { get; set; }
+
+        [DisplayName("問題10")]
+        [Required]
+        public string QUESTION10 { get; set; }
+
+        [DisplayName("問題11")]
+        [Required]
+        public string QUESTION11 { get; set; }
     }
 }
