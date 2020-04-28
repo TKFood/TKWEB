65414d9c978acabe696d83b173146cd699631dc8
jain C <at3158@hotmail.com>
2020-04-28 15:24:08 +0800

    QUESTIONNAIRES

--- TKWEB/Controllers/QUESTIONNAIRESController.cs
+++ TKWEB/Controllers/QUESTIONNAIRESController.cs
@@ -73,13 +73,14 @@ namespace TKWEB.Controllers
             // Build your models (example)
             var STATUSList = new List<SelectListItem>()
             {
-                new SelectListItem {Text="否", Value="否" },
+             
                 new SelectListItem {Text="發燒（額溫高於37.5度、耳溫高於38度、腋溫高於38度）", Value="發燒（額溫高於37.5度、耳溫高於38度、腋溫高於38度）" },
                 new SelectListItem {Text="乾咳", Value="乾咳" },
                 new SelectListItem {Text="嗅、味覺異常", Value="嗅、味覺異常" },
                 new SelectListItem {Text="呼吸急促或困難", Value="呼吸急促或困難" },
                 new SelectListItem {Text="不明原因腹瀉", Value="不明原因腹瀉" },
-                new SelectListItem {Text="有呼吸道症狀（鼻塞、流鼻水、咳嗽）", Value="有呼吸道症狀（鼻塞、流鼻水、咳嗽）" }          
+                new SelectListItem {Text="有呼吸道症狀（鼻塞、流鼻水、咳嗽）", Value="有呼吸道症狀（鼻塞、流鼻水、咳嗽）" },
+                   new SelectListItem {Text="否,以上皆無", Value="否,以上皆無" }
 
             };
             // Store your model in the ViewBag
--- TKWEB/Models/QUESTIONNAIRES.cs
+++ TKWEB/Models/QUESTIONNAIRES.cs
@@ -11,63 +11,63 @@ namespace TKWEB.Models
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
-        [DisplayName("日期")]
+        [DisplayName("1-日期(格式:西元年/月/日 2020/1/1 )")]
         [Required]
         public DateTime? DATES { get; set; }
 
-        [DisplayName("工號")]
+        [DisplayName("2-工號")]
         [Required]
         public string NO { get; set; }
 
-        [DisplayName("姓名")]
+        [DisplayName("3-姓名")]
         [Required]
         public string NAME { get; set; }
 
-        [DisplayName("部門")]
+        [DisplayName("4-部門")]
         [Required]
         public string DEP { get; set; }
 
-        [DisplayName("請問24小時內，您與您同住的家屬/室友否出現以下微狀(複選)?")]
+        [DisplayName("5-請問24小時內，您與您同住的家屬/室友是否出現以下微狀(複選)?")]
         [Required]
         public string QUESTION1 { get; set; }
 
-        [DisplayName("承上題，如有症狀請簡短說明何時、何地、何人")]
+        [DisplayName("6-承上題，如有症狀請簡短說明何時、何地、何人")]
         //[Required]
         public string QUESTION2 { get; set; }
 
-        [DisplayName("請問24小時內您與您的同住的家屬/室友是否從其他國家入境台灣？")]
+        [DisplayName("7-請問24小時內您與您的同住的家屬/室友是否從其他國家入境台灣？")]
         [Required]
         public string QUESTION3 { get; set; }
 
-        [DisplayName("承上題，簡短說明何時、何地、何人、班次?")]
+        [DisplayName("8-承上題，簡短說明何時、何地、何人、班次?")]
         //[Required]
         public string QUESTION4 { get; set; }
 
-        [DisplayName("請問24小時內您與您的同住的家屬/室友是否曾與已確診/疑似/正在接受檢驗之新型冠狀病毒肺炎病患有接觸？")]
+        [DisplayName("9-請問24小時內您與您的同住的家屬/室友是否曾與已確診/疑似/正在接受檢驗之新型冠狀病毒肺炎病患有接觸？")]
         [Required]
         public string QUESTION5 { get; set; }
 
-        [DisplayName("承上題，簡短說明何時接觸、何地接觸、何人接觸?")]
+        [DisplayName("10-承上題，簡短說明何時接觸、何地接觸、何人接觸?")]
         //[Required]
         public string QUESTION6 { get; set; }
 
-        [DisplayName("請問24小時內您與您的同住的家屬/室友是否曾前往非閉密空間但人潮擁擠的公共場所(無適當社交距離1M)，如旅遊地區、夜市、風景地區、寺廟等？")]
+        [DisplayName("11-請問24小時內您與您的同住的家屬/室友是否曾前往非閉密空間但人潮擁擠的公共場所(無適當社交距離1M)，如旅遊地區、夜市、風景地區、寺廟等？")]
         [Required]
         public string QUESTION7 { get; set; }
 
-        [DisplayName("承上題，簡短說明何時、何地、何人、共約幾人")]
+        [DisplayName("12-承上題，簡短說明何時、何地、何人、共約幾人")]
         //[Required]
         public string QUESTION8 { get; set; }
 
-        [DisplayName("請問24小時內您與您的同住的家屬/室友是否曾搭乘大眾交通運輸工具（公車、台鐵、高鐵、捷運、渡輪、客運、遊覽車…）")]
+        [DisplayName("13-請問24小時內您與您的同住的家屬/室友是否曾搭乘大眾交通運輸工具（公車、台鐵、高鐵、捷運、渡輪、客運、遊覽車…）")]
         [Required]
         public string QUESTION9 { get; set; }
 
-        [DisplayName("承上題，簡短說明何時、何地、何人、何種交通工具、班次?")]
+        [DisplayName("14-承上題，簡短說明何時、何地、何人、何種交通工具、班次?")]
         //[Required]
         public string QUESTION10 { get; set; }
 
-        [DisplayName("其他想告知的事項")]
+        [DisplayName("15-其他想告知的事項")]
         //[Required]
         public string QUESTION11 { get; set; }
 
